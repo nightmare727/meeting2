64d4abd7a0ea8e958444a013859de0ab9611b56d
ly <www.782675722.com@qq.co>
2023-12-08 18:56:26 +0800

    代码提交

--- meeting-service/src/test/java/com/tiens/meeting/dubboservice/impl/RpcMeetingResouceServiceImplTest.java
+++ meeting-service/src/test/java/com/tiens/meeting/dubboservice/impl/RpcMeetingResouceServiceImplTest.java
@@ -8,8 +8,10 @@ import com.huaweicloud.sdk.meeting.v1.model.QueryOrgVmrResultDTO;
 import com.huaweicloud.sdk.meeting.v1.model.SearchCorpVmrRequest;
 import com.huaweicloud.sdk.meeting.v1.model.SearchCorpVmrResponse;
 import com.tiens.api.service.RpcMeetingRoomService;
+import com.tiens.api.vo.MeetingResouceSelectVO;
 import com.tiens.api.vo.VMMeetingCredentialVO;
 import com.tiens.meeting.ServiceApplication;
+import com.tiens.meeting.repository.mapper.MeetingResouceMapper;
 import com.tiens.meeting.repository.po.MeetingResoucePO;
 import com.tiens.meeting.repository.service.MeetingResouceDaoService;
 import common.enums.MeetingResourceEnum;
@@ -38,6 +40,8 @@ class RpcMeetingResouceServiceImplTest {
 
     @Autowired
     MeetingResouceDaoService meetingResouceDaoService;
+    @Autowired
+    MeetingResouceMapper meetingResouceMapper;
 
     @Test
     void getClient() {
@@ -51,48 +55,53 @@ class RpcMeetingResouceServiceImplTest {
             List<QueryOrgVmrResultDTO> responseData = response.getData();
             for (QueryOrgVmrResultDTO item : responseData) {
                 System.out.println("打印打印打印打印打印打印打印item:" + item);
-                MeetingResoucePO meetingResoucePO = new MeetingResoucePO();
-                meetingResoucePO.setVmrId(item.getId());
-                meetingResoucePO.setVmrConferenceId(item.getVmrId());
-                meetingResoucePO.setVmrMode(2);
-                meetingResoucePO.setVmrName(item.getVmrName());
-                meetingResoucePO.setVmrPkgName(item.getVmrPkgName());
-                meetingResoucePO.setSize(item.getMaxAudienceParties());
+                String vmrId = item.getId();
+                MeetingResouceSelectVO meetingResouceSelectVO = meetingResouceMapper.selectVOByVMR(vmrId);
+                if (meetingResouceSelectVO == null) {
 
-                Integer status = item.getStatus();
-                if (status == 1) {
-                    meetingResoucePO.setStatus(MeetingResourceStatusEnum.MEETING_RESOURCE_STATUS_PUBLIC_FREE.getCode());
-                } else if (status == 2) {
-                    meetingResoucePO.setStatus(MeetingResourceStatusEnum.MEETING_RESOURCE_STATUS_PUBLIC_RESERVED.getCode());
-                } else if (status == 3) {
-                    meetingResoucePO.setStatus(MeetingResourceStatusEnum.MEETING_RESOURCE_STATUS_PRIVATE.getCode());
-                } else if (status == 4) {
-                    meetingResoucePO.setStatus(MeetingResourceStatusEnum.MEETING_RESOURCE_STATUS_PUBLIC_PRE_ALLOCATED.getCode());
+                    MeetingResoucePO meetingResoucePO = new MeetingResoucePO();
+                    meetingResoucePO.setVmrId(item.getId());
+                    meetingResoucePO.setVmrConferenceId(item.getVmrId());
+                    meetingResoucePO.setVmrMode(2);
+                    meetingResoucePO.setVmrName(item.getVmrName());
+                    meetingResoucePO.setVmrPkgName(item.getVmrPkgName());
+                    meetingResoucePO.setSize(item.getMaxAudienceParties());
+
+                    Integer status = item.getStatus();
+                    if (status == 1) {
+                        meetingResoucePO.setStatus(MeetingResourceStatusEnum.MEETING_RESOURCE_STATUS_PUBLIC_FREE.getCode());
+                    } else if (status == 2) {
+                        meetingResoucePO.setStatus(MeetingResourceStatusEnum.MEETING_RESOURCE_STATUS_PUBLIC_RESERVED.getCode());
+                    } else if (status == 3) {
+                        meetingResoucePO.setStatus(MeetingResourceStatusEnum.MEETING_RESOURCE_STATUS_PRIVATE.getCode());
+                    } else if (status == 4) {
+                        meetingResoucePO.setStatus(MeetingResourceStatusEnum.MEETING_RESOURCE_STATUS_PUBLIC_PRE_ALLOCATED.getCode());
+                    }
+
+                    Integer vmrPkgParties = item.getMaxAudienceParties();
+                    if (vmrPkgParties == MeetingResourceEnum.MEETING_RESOURCE_10.getValue()) {
+                        meetingResoucePO.setResourceType(MeetingResourceEnum.MEETING_RESOURCE_10.getCode());
+                    } else if (vmrPkgParties == MeetingResourceEnum.MEETING_RESOURCE_50.getValue()) {
+                        meetingResoucePO.setResourceType(MeetingResourceEnum.MEETING_RESOURCE_50.getCode());
+                    } else if (vmrPkgParties == MeetingResourceEnum.MEETING_RESOURCE_100.getValue()) {
+                        meetingResoucePO.setResourceType(MeetingResourceEnum.MEETING_RESOURCE_100.getCode());
+                    } else if (vmrPkgParties == MeetingResourceEnum.MEETING_RESOURCE_200.getValue()) {
+                        meetingResoucePO.setResourceType(MeetingResourceEnum.MEETING_RESOURCE_200.getCode());
+                    } else if (vmrPkgParties == MeetingResourceEnum.MEETING_RESOURCE_500.getValue()) {
+                        meetingResoucePO.setResourceType(MeetingResourceEnum.MEETING_RESOURCE_500.getCode());
+                    } else if (vmrPkgParties == MeetingResourceEnum.MEETING_RESOURCE_1000.getValue()) {
+                        meetingResoucePO.setResourceType(MeetingResourceEnum.MEETING_RESOURCE_1000.getCode());
+                    } else if (vmrPkgParties == MeetingResourceEnum.MEETING_RESOURCE_3000.getValue()) {
+                        meetingResoucePO.setResourceType(MeetingResourceEnum.MEETING_RESOURCE_3000.getCode());
+                    }
+
+
+                    Date date = new Date(item.getExpireDate());
+                    meetingResoucePO.setExpireDate(date);
+                    System.out.println("打印打印meetingResoucePO:" + meetingResoucePO);
+                    meetingResouceDaoService.save(meetingResoucePO);
+
                 }
-
-                Integer vmrPkgParties = item.getMaxAudienceParties();
-                if (vmrPkgParties == MeetingResourceEnum.MEETING_RESOURCE_10.getValue()) {
-                    meetingResoucePO.setResourceType(MeetingResourceEnum.MEETING_RESOURCE_10.getCode());
-                } else if (vmrPkgParties == MeetingResourceEnum.MEETING_RESOURCE_50.getValue()) {
-                    meetingResoucePO.setResourceType(MeetingResourceEnum.MEETING_RESOURCE_50.getCode());
-                } else if (vmrPkgParties == MeetingResourceEnum.MEETING_RESOURCE_100.getValue()) {
-                    meetingResoucePO.setResourceType(MeetingResourceEnum.MEETING_RESOURCE_100.getCode());
-                } else if (vmrPkgParties == MeetingResourceEnum.MEETING_RESOURCE_200.getValue()) {
-                    meetingResoucePO.setResourceType(MeetingResourceEnum.MEETING_RESOURCE_200.getCode());
-                } else if (vmrPkgParties == MeetingResourceEnum.MEETING_RESOURCE_500.getValue()) {
-                    meetingResoucePO.setResourceType(MeetingResourceEnum.MEETING_RESOURCE_500.getCode());
-                } else if (vmrPkgParties == MeetingResourceEnum.MEETING_RESOURCE_1000.getValue()) {
-                    meetingResoucePO.setResourceType(MeetingResourceEnum.MEETING_RESOURCE_1000.getCode());
-                } else if (vmrPkgParties == MeetingResourceEnum.MEETING_RESOURCE_3000.getValue()) {
-                    meetingResoucePO.setResourceType(MeetingResourceEnum.MEETING_RESOURCE_3000.getCode());
-                }
-
-
-                Date date = new Date(item.getExpireDate());
-                meetingResoucePO.setExpireDate(date);
-                System.out.println("打印打印meetingResoucePO:"+meetingResoucePO);
-                meetingResouceDaoService.save(meetingResoucePO);
-
             }
             System.out.println("打印response:" + response.toString());
 
@@ -105,5 +114,23 @@ class RpcMeetingResouceServiceImplTest {
             //System.out.println(e.getErrorCode());
             //System.out.println(e.getErrorMsg());
         }
+    }
+
+    @Test
+    void assignMeetingResouce() {
+        //更改本地数据库分配会议资源操作
+        int i = meetingResouceDaoService.assignMeetingResouce("2bca6344aedc444598cdc76a4adc8f22");
+        System.out.println("打印打印打印:" + i);
+    }
+
+
+    @Test
+    void updateStatusByAccId() {
+        meetingResouceMapper.updateStatusByAccId("2bca6344aedc444598cdc76a4adc8f22");
+    }
+
+    @Test
+    void updateMeetingResourceStatusPrivate() {
+        meetingResouceMapper.updateMeetingResourceStatusPrivate("2bca6344aedc444598cdc76a4adc8f22");
     }
 }
