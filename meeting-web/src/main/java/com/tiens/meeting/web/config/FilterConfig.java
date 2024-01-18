524babc035f1bf5717462f025a6373f2460f326e
yuwenjie <weiwj1@tiens.com>
2024-01-18 08:58:19 +0800

    修改过滤器注入配置

--- meeting-web/src/main/java/com/tiens/meeting/web/config/FilterConfig.java
+++ meeting-web/src/main/java/com/tiens/meeting/web/config/FilterConfig.java
@@ -1,10 +1,5 @@
 package com.tiens.meeting.web.config;
 
-import cn.hutool.crypto.SecureUtil;
-import cn.hutool.crypto.asymmetric.Sign;
-import cn.hutool.crypto.asymmetric.SignAlgorithm;
-import cn.hutool.crypto.digest.DigestAlgorithm;
-import com.google.common.collect.ImmutableMap;
 import com.tiens.meeting.web.filter.AuthFilter;
 import com.tiens.meeting.web.filter.HeaderResolveFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -22,9 +17,9 @@ public class FilterConfig {
      */
 
     @Bean
-    public FilterRegistrationBean<HeaderResolveFilter> headerResolveFilter() {
+    public FilterRegistrationBean<HeaderResolveFilter> filterRegistrationBeanHeaderResolveFilter() {
         FilterRegistrationBean<HeaderResolveFilter> filter = new FilterRegistrationBean();
-        filter.setFilter(new HeaderResolveFilter());
+        filter.setFilter(headerResolveFilter());
         filter.addUrlPatterns("/*");
         filter.setName("headerResolveFilter");
         filter.setOrder(FilterRegistrationBean.HIGHEST_PRECEDENCE);
@@ -32,15 +27,23 @@ public class FilterConfig {
     }
 
     @Bean
-    public FilterRegistrationBean<AuthFilter> authFilter() {
+    public FilterRegistrationBean<AuthFilter> filterRegistrationBeanAuthFilter() {
         FilterRegistrationBean<AuthFilter> filter = new FilterRegistrationBean();
-        filter.setFilter(new AuthFilter());
+        filter.setFilter(authFilter());
         filter.addUrlPatterns("/mtuser/addMeetingHostUser");
         filter.setName("authFilter");
         filter.setOrder(1);
         return filter;
     }
 
+    @Bean("authFilter")
+    AuthFilter authFilter() {
+        return new AuthFilter();
+    }
 
+    @Bean("headerResolveFilter")
+    HeaderResolveFilter headerResolveFilter() {
+        return new HeaderResolveFilter();
+    }
 
 }
