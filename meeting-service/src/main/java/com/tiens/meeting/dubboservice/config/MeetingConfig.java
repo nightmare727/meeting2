c2d43bb8971623d27e0c5bf74d61e8b776dda61b
yuwenjie <weiwj1@tiens.com>
2024-01-26 16:11:11 +0800

    增加配置动态刷新

--- meeting-service/src/main/java/com/tiens/meeting/dubboservice/config/MeetingConfig.java
+++ meeting-service/src/main/java/com/tiens/meeting/dubboservice/config/MeetingConfig.java
@@ -2,12 +2,14 @@ package com.tiens.meeting.dubboservice.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
 @Configuration
 @Data
+@RefreshScope
 @ConfigurationProperties(prefix = "meeting")
 public class MeetingConfig {
 
