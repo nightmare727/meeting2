0aa4396480a29fcf7d5d963470dd11b13341e5e4
yuwenjie <18210515314@163.com>
2024-04-19 00:30:46 +0800

    私有会议邀请信息为资源会议号

--- meeting-service/src/main/java/com/tiens/meeting/dubboservice/async/RoomAsyncTask.java
+++ meeting-service/src/main/java/com/tiens/meeting/dubboservice/async/RoomAsyncTask.java
@@ -1,6 +1,7 @@
 package com.tiens.meeting.dubboservice.async;
 
 import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.NumberUtil;
 import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONObject;
@@ -18,8 +19,10 @@ import com.tiens.meeting.dubboservice.bo.PushMessageDto;
 import com.tiens.meeting.dubboservice.config.MeetingConfig;
 import com.tiens.meeting.dubboservice.core.LanguageService;
 import com.tiens.meeting.repository.po.MeetingHwEventCallbackPO;
+import com.tiens.meeting.repository.po.MeetingResourcePO;
 import com.tiens.meeting.repository.po.MeetingRoomInfoPO;
 import com.tiens.meeting.repository.service.MeetingHwEventCallbackDaoService;
+import com.tiens.meeting.repository.service.MeetingResourceDaoService;
 import common.util.date.DateUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -51,8 +54,8 @@ public class RoomAsyncTask implements RoomAsyncTaskService {
 
     private final MeetingHwEventCallbackDaoService meetingHwEventCallbackDaoService;
 
-//    @Reference(version = "1.0")
-//    MessageService messageService;
+    // @Reference(version = "1.0")
+    // MessageService messageService;
 
     @Autowired
     RocketMQTemplate rocketMQTemplate;
@@ -65,6 +68,9 @@ public class RoomAsyncTask implements RoomAsyncTaskService {
 
     @Autowired
     LanguageService languageService;
+
+    @Autowired
+    MeetingResourceDaoService meetingResourceDaoService;
 
     /**
      * 保存回调记录
@@ -98,80 +104,98 @@ public class RoomAsyncTask implements RoomAsyncTaskService {
     @Override
     public void batchSendIMMessage(MeetingRoomInfoPO meetingRoomInfoPO, List<String> toAccIds) {
         log.info("【批量发送点对点IM消息】 会议入参：{}，接收人：{}", meetingRoomInfoPO, toAccIds);
+        try {
+            String languageId = meetingRoomInfoPO.getLanguageId();
+            /**
+             * 消息内容，最大长度 5000 字符，JSON 格式
+             */
 
-        String languageId = meetingRoomInfoPO.getLanguageId();
-        /**
-         * 消息内容，最大长度 5000 字符，JSON 格式
-         */
-
-        HashMap<@Nullable String, @Nullable Object> body = Maps.newHashMap();
-/*
-        JSONObject roomPush = JSONUtil.createObj().set("subject", meetingRoomInfoPO.getSubject())
+            HashMap<@Nullable String, @Nullable Object> body = Maps.newHashMap();
+            /*
+            JSONObject roomPush = JSONUtil.createObj().set("subject", meetingRoomInfoPO.getSubject())
             .set("meetingCode", meetingRoomInfoPO.getHwMeetingCode())
             .set("startTime", DateUtil.formatDateTime(meetingRoomInfoPO.getShowStartTime()))*/
-        ;
-        SimpleDateFormat YMDFormat = new SimpleDateFormat("yyyy/MM/dd");
-        SimpleDateFormat HMFormat = new SimpleDateFormat("HH:mm");
-        //邀请密码
-        String invitePwd =
-            ObjectUtil.defaultIfBlank(meetingRoomInfoPO.getGeneralPwd(), meetingRoomInfoPO.getAudiencePasswd());
-        String timeZoneOffset = meetingRoomInfoPO.getTimeZoneOffset();
+            ;
+            String hwMeetingCode = meetingRoomInfoPO.getHwMeetingCode();
 
-        String meetingTime = DateUtil.format(
-            DateUtils.convertTimeZone(meetingRoomInfoPO.getShowStartTime(), DateUtils.TIME_ZONE_GMT,
-                ZoneId.of(timeZoneOffset)), YMDFormat) + " " + DateUtil.format(
-            DateUtils.convertTimeZone(meetingRoomInfoPO.getShowStartTime(), DateUtils.TIME_ZONE_GMT,
-                ZoneId.of(timeZoneOffset)), HMFormat) + "-" + DateUtil.format(
-            DateUtils.convertTimeZone(meetingRoomInfoPO.getShowEndTime(), DateUtils.TIME_ZONE_GMT,
-                ZoneId.of(timeZoneOffset)), HMFormat) + "(" + timeZoneOffset + ")";
+            if (!NumberUtil.isNumber(meetingRoomInfoPO.getResourceType())) {
+                MeetingResourcePO one = meetingResourceDaoService.lambdaQuery()
+                    .eq(MeetingResourcePO::getId, meetingRoomInfoPO.getResourceId()).one();
+                hwMeetingCode = one.getVmrConferenceId();
 
-        JSONObject pushData = JSONUtil.createObj().set("contentImage", meetingConfig.getMeetingIcon())
-            .set("contentStr", languageService.getLanguageValue(languageId, meetingConfig.getInviteContentKey()))
-            .set("im_prefix", languageService.getLanguageValue(languageId, meetingConfig.getInviteImPrefixContentKey()))
-            .set("landingType", 2).set("landingUrl", "TencentMeetingPage").set("contentSubTitle",
-                languageService.getLanguageValue(languageId,
-                    meetingConfig.getMeetingTitleKey()) + "：" + meetingRoomInfoPO.getSubject() + "\n" + languageService.getLanguageValue(
-                    languageId,
-                    meetingConfig.getMeetingTimeKey()) + "：" + meetingTime + "\n" + languageService.getLanguageValue(
-                    languageId, meetingConfig.getMeetingCodeKey()) + "：" + meetingRoomInfoPO.getHwMeetingCode() + (
-                    StrUtil.isNotBlank(invitePwd) ? "\n" + languageService.getLanguageValue(languageId,
-                        meetingConfig.getMeetingPwdKey()) + "：" + invitePwd : ""));
+            }
 
-        body.put("type", "212");
-        body.put("data", pushData);
-        body.put("pushTitle",
-            languageService.getLanguageValue(languageId, meetingConfig.getMeetingInvitePushSubTitleKey()));
-        /**
-         * 必须是JSON,不能超过2k字符。该参数与APNs推送的payload含义不同
-         */
-        MessagePayloadDTO messagePayloadDTO = new MessagePayloadDTO(body);
-        ArrayList<@Nullable LanguageWordBO> languageWordBOS = Lists.newArrayList();
-        //邀请您参加V-Meeting会议
-        languageWordBOS.add(new LanguageWordBO(meetingConfig.getInviteTopicKey(), ""));
-        //会议主题
-        languageWordBOS.add(new LanguageWordBO(meetingConfig.getMeetingTitleKey(), meetingRoomInfoPO.getSubject()));
-        //会议时间
-        languageWordBOS.add(new LanguageWordBO(meetingConfig.getMeetingTimeKey(), meetingTime));
-        //会议号
-        languageWordBOS.add(
-            new LanguageWordBO(meetingConfig.getMeetingCodeKey(), meetingRoomInfoPO.getHwMeetingCode()));
+            SimpleDateFormat YMDFormat = new SimpleDateFormat("yyyy/MM/dd");
+            SimpleDateFormat HMFormat = new SimpleDateFormat("HH:mm");
+            // 邀请密码
+            String invitePwd =
+                ObjectUtil.defaultIfBlank(meetingRoomInfoPO.getGeneralPwd(), meetingRoomInfoPO.getAudiencePasswd());
+            String timeZoneOffset = meetingRoomInfoPO.getTimeZoneOffset();
 
-        PushMessageDto pushMessageDto = new PushMessageDto();
-        pushMessageDto.setAccId(meetingRoomInfoPO.getOwnerImUserId());
-        pushMessageDto.setTo(toAccIds);
-        pushMessageDto.setMsgType(1);
-        pushMessageDto.setTitle(meetingConfig.getInviteContentKey());
-        pushMessageDto.setContent(languageWordBOS);
-        pushMessageDto.setBody(body);
-        pushMessageDto.setPayload(messagePayloadDTO);
-        pushMessageDto.setPushContent(
-            languageService.getLanguageValue(languageId, meetingConfig.getInviteContentKey()));
-        pushMessageDto.setSubtype(1);
-        Message<String> message = MessageBuilder.withPayload(
-            JSON.toJSONString(pushMessageDto, SerializerFeature.DisableCircularReferenceDetect)).build();
-        log.info("【批量发送点对点IM消息】调用入参：{}", JSON.toJSONString(pushMessageDto));
-        SendResult sendResult = rocketMQTemplate.syncSend(pushMessageTopic, message);
-        log.info("【批量发送点对点IM消息】结果返回：{}", JSON.toJSONString(sendResult));
+            String meetingTime = DateUtil
+                .format(DateUtils.convertTimeZone(meetingRoomInfoPO.getShowStartTime(), DateUtils.TIME_ZONE_GMT,
+                    ZoneId.of(timeZoneOffset)), YMDFormat)
+                + " "
+                + DateUtil.format(DateUtils.convertTimeZone(meetingRoomInfoPO.getShowStartTime(),
+                    DateUtils.TIME_ZONE_GMT, ZoneId.of(timeZoneOffset)), HMFormat)
+                + "-" + DateUtil.format(DateUtils.convertTimeZone(meetingRoomInfoPO.getShowEndTime(),
+                    DateUtils.TIME_ZONE_GMT, ZoneId.of(timeZoneOffset)), HMFormat)
+                + "(" + timeZoneOffset + ")";
+
+            JSONObject pushData = JSONUtil.createObj().set("contentImage", meetingConfig.getMeetingIcon())
+                .set("contentStr", languageService.getLanguageValue(languageId, meetingConfig.getInviteContentKey()))
+                .set("im_prefix",
+                    languageService.getLanguageValue(languageId, meetingConfig.getInviteImPrefixContentKey()))
+                .set("landingType", 2).set("landingUrl", "TencentMeetingPage").set("contentSubTitle",
+                    languageService.getLanguageValue(languageId, meetingConfig.getMeetingTitleKey()) + "："
+                        + meetingRoomInfoPO.getSubject() + "\n"
+                        + languageService.getLanguageValue(languageId, meetingConfig.getMeetingTimeKey()) + "："
+                        + meetingTime + "\n"
+                        + languageService.getLanguageValue(languageId, meetingConfig.getMeetingCodeKey()) + "："
+                        + hwMeetingCode
+                        + (StrUtil.isNotBlank(invitePwd)
+                            ? "\n" + languageService.getLanguageValue(languageId, meetingConfig.getMeetingPwdKey())
+                                + "：" + invitePwd
+                            : ""));
+
+            body.put("type", "212");
+            body.put("data", pushData);
+            body.put("pushTitle",
+                languageService.getLanguageValue(languageId, meetingConfig.getMeetingInvitePushSubTitleKey()));
+            /**
+             * 必须是JSON,不能超过2k字符。该参数与APNs推送的payload含义不同
+             */
+            MessagePayloadDTO messagePayloadDTO = new MessagePayloadDTO(body);
+            ArrayList<@Nullable LanguageWordBO> languageWordBOS = Lists.newArrayList();
+            // 邀请您参加V-Meeting会议
+            languageWordBOS.add(new LanguageWordBO(meetingConfig.getInviteTopicKey(), ""));
+            // 会议主题
+            languageWordBOS.add(new LanguageWordBO(meetingConfig.getMeetingTitleKey(), meetingRoomInfoPO.getSubject()));
+            // 会议时间
+            languageWordBOS.add(new LanguageWordBO(meetingConfig.getMeetingTimeKey(), meetingTime));
+            // 会议号
+            languageWordBOS.add(new LanguageWordBO(meetingConfig.getMeetingCodeKey(), hwMeetingCode));
+
+            PushMessageDto pushMessageDto = new PushMessageDto();
+            pushMessageDto.setAccId(meetingRoomInfoPO.getOwnerImUserId());
+            pushMessageDto.setTo(toAccIds);
+            pushMessageDto.setMsgType(1);
+            pushMessageDto.setTitle(meetingConfig.getInviteContentKey());
+            pushMessageDto.setContent(languageWordBOS);
+            pushMessageDto.setBody(body);
+            pushMessageDto.setPayload(messagePayloadDTO);
+            pushMessageDto
+                .setPushContent(languageService.getLanguageValue(languageId, meetingConfig.getInviteContentKey()));
+            pushMessageDto.setSubtype(1);
+            Message<String> message = MessageBuilder
+                .withPayload(JSON.toJSONString(pushMessageDto, SerializerFeature.DisableCircularReferenceDetect))
+                .build();
+            log.info("【批量发送点对点IM消息】调用入参：{}", JSON.toJSONString(pushMessageDto));
+            SendResult sendResult = rocketMQTemplate.syncSend(pushMessageTopic, message);
+            log.info("【批量发送点对点IM消息】结果返回：{}", JSON.toJSONString(sendResult));
+        } catch (Exception e) {
+            log.error("异步邀请与会者异常", e);
+        }
 
     }
 
