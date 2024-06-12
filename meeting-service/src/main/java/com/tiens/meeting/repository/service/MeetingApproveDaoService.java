a7e620a81adaaf85b41bd6b341789a2fb68209c9
yuwenjie <weiwj1@tiens.com>
2024-06-12 15:06:02 +0800

    完成大型会议申请

---
+++ meeting-service/src/main/java/com/tiens/meeting/repository/service/MeetingApproveDaoService.java
@@ -0,0 +1,13 @@
+package com.tiens.meeting.repository.service;
+
+import com.tiens.meeting.repository.po.MeetingApprovePO;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author yuwenjie
+* @description 针对表【meeting_approve(大型会议审批表)】的数据库操作Service
+* @createDate 2024-06-12 12:01:28
+*/
+public interface MeetingApproveDaoService extends IService<MeetingApprovePO> {
+
+}
---
+++ meeting-service/src/main/java/com/tiens/meeting/repository/service/impl/MeetingApproveDaoServiceImpl.java
@@ -0,0 +1,22 @@
+package com.tiens.meeting.repository.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.tiens.meeting.repository.po.MeetingApprovePO;
+import com.tiens.meeting.repository.service.MeetingApproveDaoService;
+import com.tiens.meeting.repository.mapper.MeetingApproveMapper;
+import org.springframework.stereotype.Service;
+
+/**
+* @author yuwenjie
+* @description 针对表【meeting_approve(大型会议审批表)】的数据库操作Service实现
+* @createDate 2024-06-12 12:01:28
+*/
+@Service
+public class MeetingApproveDaoServiceImpl extends ServiceImpl<MeetingApproveMapper, MeetingApprovePO>
+    implements MeetingApproveDaoService {
+
+}
+
+
+
+
