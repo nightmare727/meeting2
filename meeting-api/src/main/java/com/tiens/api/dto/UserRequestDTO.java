aa487f2d2c497e72e4ef157691cff0e14398a237
zhankui <zhankui@tiens.com>
2024-08-08 14:25:07 +0800

    fix

--- meeting-api/src/main/java/com/tiens/api/dto/UserRequestDTO.java
+++ meeting-api/src/main/java/com/tiens/api/dto/UserRequestDTO.java
@@ -1,5 +1,6 @@
 package com.tiens.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,11 +8,13 @@ import lombok.NoArgsConstructor;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserRequestDTO implements Serializable {
     private List<String> userIdList;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date endTime;
 }
