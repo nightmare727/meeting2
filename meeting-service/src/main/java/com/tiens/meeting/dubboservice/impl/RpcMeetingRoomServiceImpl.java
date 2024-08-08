881b7a9432109ef325d587d843f77d49463fdff4
zhankui <zhankui@tiens.com>
2024-08-08 17:28:22 +0800

    换id

--- meeting-service/src/main/java/com/tiens/meeting/dubboservice/impl/RpcMeetingRoomServiceImpl.java
+++ meeting-service/src/main/java/com/tiens/meeting/dubboservice/impl/RpcMeetingRoomServiceImpl.java
@@ -643,7 +643,7 @@ public class RpcMeetingRoomServiceImpl implements RpcMeetingRoomService {
     private CommonResult checkCreateMeetingRoom(MeetingRoomContextDTO meetingRoomContextDTO) {
 
         // 判断是否在黑名单
-        RBucket<MeetingBlackUserVO> bucket = redissonClient.getBucket(CacheKeyUtil.getBlackUserInfoKey(meetingRoomContextDTO.getJoyoCode()));
+        RBucket<MeetingBlackUserVO> bucket = redissonClient.getBucket(CacheKeyUtil.getBlackUserInfoKey(meetingRoomContextDTO.getImUserId()));
         if (bucket.isExists()) {
             return CommonResult.error(GlobalErrorCodeConstants.USER_IN_BLACK_LIST);
         }
@@ -780,7 +780,7 @@ public class RpcMeetingRoomServiceImpl implements RpcMeetingRoomService {
     }
 
     private CommonResult checkUpdateMeetingRoom(MeetingRoomContextDTO meetingRoomContextDTO) {
-        RBucket<MeetingBlackUserVO> bucket = redissonClient.getBucket(CacheKeyUtil.getBlackUserInfoKey(meetingRoomContextDTO.getJoyoCode()));
+        RBucket<MeetingBlackUserVO> bucket = redissonClient.getBucket(CacheKeyUtil.getBlackUserInfoKey(meetingRoomContextDTO.getImUserId()));
         if (bucket.isExists()) {
             return CommonResult.error(GlobalErrorCodeConstants.USER_IN_BLACK_LIST);
         }
